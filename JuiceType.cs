a4df5f92ce5c244e7f1044e005ddc2cfb1485d19
nafef60552 <nafef60552@qianhost.com>
2023-10-30 19:34:22 +0200

    Tsebro did 1 task

---
+++ JuiceType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    enum JuiceType
+    {
+        UNKNOWN,
+        APPLE,
+        PEAR,
+        ORANGE,
+        MULTIVITAMIN,
+        TOMATO,
+    }
+}
